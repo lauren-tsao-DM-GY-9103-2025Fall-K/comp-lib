6480e18beff26644c8eb74dd570b57606fdead56
Lauren Tsao <et2676@nyu.edu>
2025-09-16 16:41:13 -0400

    button notes from WK03

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // I want this library to be available for the following jsx, jsx etc
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
